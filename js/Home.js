a55780e97f8e3d6fcd57b23194153ab44504f2ed
Vikash <vikashpathak041@gmail.com>
2022-07-21 14:33:46 +0530

    uc  completed

--- js/Home.js
+++ js/Home.js
@@ -44,3 +44,15 @@ const getDeptHtml = (deptList) => {
     }
     return deptHtml;
 }
+
+const remove = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._id == node.id);
+    if(!empPayrollData) return;
+    const index = empPayrollList
+                    .map(empData => empData._id)
+                    .indexOf(empPayrollData._id);
+    empPayrollList.splice(index, 1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+    document.querySelector('.emp-count').textContent = empPayrollList.length;
+    createInnerHtml();
+}
